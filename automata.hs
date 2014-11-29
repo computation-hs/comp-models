1dc8f6b511cea7f81b0d9814c858be10b36fbfa6
Pablo <pbaeyens@users.noreply.github.com>
2014-11-29 11:49:52 +0100

    [automata.hs] Ejecución de NFA
    Funciona!

--- automata.hs
+++ automata.hs
@@ -3,6 +3,7 @@ import Control.Monad.Writer
 --- Tipos ---
 
 type State = String
+data WriterList a b = WriterList {runWriterList :: WriterT b [] a}
 
 data DFA = DFA
     { initial :: State
@@ -38,18 +39,26 @@ dfa = DFA i f t
 --- NFA de ejemplo ---
 
 t' :: State -> Char -> [State]
-t' "1" 'A' = ["2"]
+t' "1" 'A' = ["2","4"]
 t' "2" 'A' = ["3"]
 t' "2" 'B' = ["1"]
 t' "2" 'C' = ["3"]
 t' "3" 'D' = ["5"]
 t' "4" 'A' = ["2"]
+t' _   _   = []
 
 nfa = NFA i f t'
 
 --- Funciones con log ---
 
-logt t s c = writer ((t s c), [s])
-execution (DFA i f t) = execWriter . foldM (logt t) i
+logDFA t s c = writer (t s c, [s])
+executeDFA (DFA i f t) = execWriter . foldM (logDFA t) i
 
-main = putStrLn $ show $ execution dfa ['A','A','D']
+
+logNFA t s c = WriterT $ map (\x -> (x,[s])) (t s c)
+executeNFA (NFA i f t) = execWriterT . foldM (logNFA t) i
+
+
+--- Main e IO ---
+
+main = sequence_ [putStrLn $ show $ executeDFA dfa ['A','A','D'], putStrLn $ show $ executeNFA nfa ['A','A'] ]
