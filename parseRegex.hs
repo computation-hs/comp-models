8e09d3a7036b56fce853dcfc6db4c93c3e8b0bd2
M42 <mromang08@gmail.com>
2014-12-12 00:04:22 +0100

    Simplifies the parser code Adds type declarations and removes unused libreries

--- parseRegex.hs
+++ parseRegex.hs
@@ -2,28 +2,30 @@ import Regex
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Expr
---import Text.Parsec.Token
 import Control.Applicative ((<$>))
-import Text.Parsec.Token as P
-import Text.Parsec.Language (haskellDef)
 
 {- 
   Syntax for a Regex parser
 -}
 
+regexp :: Parser Regex
 regexp = buildExpressionParser optable term
 
 optable = [ [Infix concatp AssocLeft ]
           , [Infix alternp AssocLeft ]
           ]
 
-term = literalp <|> Main.parens regexp
+term :: Parser Regex
+term = literalp <|> parens regexp
+
+literalp :: Parser Regex
 literalp = Literal <$> lower
 
-alternp = string "+" >> return (Altern)
-concatp = string "." >> return (Concat)
+alternp :: Parser (Regex -> Regex -> Regex)
+alternp = char '+' >> return (Altern)
 
+concatp :: Parser (Regex -> Regex -> Regex)
+concatp = char '.' >> return (Concat)
 
--- parens (?)
-lexer = P.makeTokenParser haskellDef
-parens = P.parens lexer
+parens :: Parser a -> Parser a
+parens = between (char '(') (char ')')
