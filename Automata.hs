5c0bf8d2bfb8c79ff3999cb0def3b8ff6233e748
pbaeyens <pbaeyens31@gmail.com>
2014-12-18 19:22:15 +0100

    Renamed files. Added Maybe to DFAs.

--- Automata.hs
+++ Automata.hs
@@ -1,32 +1,25 @@
 import Control.Monad.Writer
+import Data.Maybe
 
 --- Basic types for automata ---
 
 type State = String -- ^ Type synonym for automata states.
 type Alpha = Char   -- ^ Type synonym for automata symbols.
 
-data DFA = DFA
-    { initial :: State
-    , isFinal :: State -> Bool
-    , transition :: State -> Alpha -> State
-    }
-
-data NFA = NFA
-    { ini :: State
-    , fin :: State -> Bool
-    , tra :: State -> Alpha -> [State]
-    }
+data DFA = DFA State (State -> Bool) (State -> Alpha -> Maybe State)
+data NFA = NFA State (State -> Bool) (State -> Alpha -> [State])
 
 
 --- Example DFA ---
 
-t :: State -> Alpha -> State
-t "1" 'A' = "2"
-t "2" 'A' = "3"
-t "2" 'B' = "1"
-t "2" 'C' = "3"
-t "3" 'D' = "5"
-t "4" 'A' = "2"
+t :: State -> Alpha -> Maybe State
+t "1" 'A' = Just "2"
+t "2" 'A' = Just "3"
+t "2" 'B' = Just "1"
+t "2" 'C' = Just "3"
+t "3" 'D' = Just "5"
+t "4" 'A' = Just "2"
+t  _   _  = Nothing
 
 i :: State
 i = "1"
@@ -52,24 +45,34 @@ nfa = NFA i f t'
 
 --- Automata execution functions ---
 
-process :: DFA -> [Alpha] -> State
+--process :: DFA -> [Alpha] -> State
 -- ^ Returns the final state of a DFA given a word.
-process (DFA i f t) = foldl t i
+process (DFA i f t) = foldM t i
 
 accept :: DFA -> [Alpha] -> Bool
 -- ^ Checks if a certain word is accepted by a given DFA.
-accept dfa@(DFA _ f _) = f . (process dfa) 
-
+accept dfa@(DFA _ f _) xs =
+  if isJust final
+    then f $ fromJust final
+    else False
+  where final = process dfa xs
 
 --- Automata log functions ---
 
-logDFA t s c = writer (t s c, [s])
+logDFA t s c =
+  WriterT $
+  if isJust state
+    then Just (fromJust state, [s])
+    else Nothing
+  where state = t s c
+
 logNFA t s c = WriterT $ map (\x -> (x,[s])) (t s c)
 addFinal (a,xs) = xs ++ [a]
 
-executeDFA :: DFA -> [Alpha] -> [State]
+
+executeDFA :: DFA -> [Alpha] -> Maybe [State]
 -- ^ Returns the execution log of DFA given a word.
-executeDFA (DFA i _ t) = addFinal . runWriter . foldM (logDFA t) i
+executeDFA (DFA i _ t) = (maybe Nothing (Just . addFinal)) . runWriterT . foldM (logDFA t) i
 
 executeNFA :: NFA -> [Alpha] -> [[State]]
 -- ^ Returns the executions logs of all paths in a NFA given a word.
