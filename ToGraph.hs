7e9868734ab7b04fd5a1619c4d2459e739ea27c0
M42 <mromang08@gmail.com>
2015-01-08 11:55:58 +0100

    ToDot function Outputs a graph enconding the DFA. Format and other minor issues will have to be changed.

--- ToGraph.hs
+++ ToGraph.hs
@@ -3,7 +3,7 @@ module ToGraph where
 import Automata
 import ToString
 import Data.Maybe
-
+import Data.List
 
 header = "digraph {"
 end    = "}"
@@ -17,11 +17,23 @@ converge (x:ys@(y:_))
 stabilize :: (Eq a) => (a -> a) -> (a -> a)
 stabilize f x = converge $ iterate f x
 
+-- Lists states
 listStates :: DFA -> [State]
-listStates dfa = stabilize (concat . (map (newStates dfa))) [fromJust (initialDFA dfa)]
+listStates dfa = stabilize (nub . concat . (map (newStates dfa))) [fromJust (initialDFA dfa)]
 
 newStates :: DFA -> State -> [State]
-newStates dfa s = [fromJust ((deltaDFA dfa) s a) | a <- (alphaDFA dfa)]
+newStates dfa s = s : catMaybes [(deltaDFA dfa) s a | a <- (alphaDFA dfa)]
+
+
+-- Writes graph
+toDot :: DFA -> [String]
+toDot dfa = [case delta s a of
+               Nothing -> ""
+               Just t  -> toString s ++ "," ++ toString a ++ "," ++ toString t
+             | a <- alpha, s <- states]
+      where alpha  = alphaDFA dfa
+            states = listStates dfa
+            delta  = deltaDFA dfa
 
 {-
 toGraph :: DFA -> String
