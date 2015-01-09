2b33990d0888445fd614954c528a9a7d9706cdbc
M42 <mromang08@gmail.com>
2015-01-09 15:55:58 +0100

    Cleans ToGraph.hs

--- ToGraph.hs
+++ ToGraph.hs
@@ -17,13 +17,6 @@ instance (Automata a m) => IsGraph a where
     toDot = nfaToDot . toNFA
 
 
--- Auxiliary function. Lists states.
-listStatesNfa :: NFA -> [State]
-listStatesNfa nfa = stabilize (nub . concat . (map (newStatesNfa nfa))) (initial nfa)
-
-newStatesNfa :: NFA -> State -> [State]
-newStatesNfa nfa s = s : concat [(delta nfa) s a | a <- (alpha nfa)]
-
 -- Writes graph
 -- | 'toDot' returns a formatted string, containing the representation of the
 --   automata as a .dot graph.
@@ -40,9 +33,9 @@ nfaToDot nfa = header ++
 --    graph of the automata. An edge will represent a transition and will be 
 --    labeled with the alphabet input.
 edges :: NFA -> [String]
-edges nfa = concat [fmap (format s a) (delt s a) | a <- alph, s <- states]
-    where alph   = alpha nfa
-          states = listStatesNfa nfa
-          delt   = delta nfa
+edges nfa = concat [fmap (format s a) (delt s a) | a <- alph, s <- stat]
+    where alph = alpha nfa
+          stat = states nfa
+          delt = delta nfa
           format s a t = "\t" ++ toString s ++ " -> " ++ toString t 
                               ++ " label[\"" ++ toString a ++ "\"]"
